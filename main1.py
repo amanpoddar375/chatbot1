2313d29a8bce4aea82454b46e3535e8acb8046f8
Aman Poddar <amanpoddar5566@gmail.com>
2024-02-09 15:59:30 +0545

    first commit

---
+++ main1.py
@@ -0,0 +1,24 @@
+import os
+from llama_index.llms import OpenAI
+from llama_index import VectorStoreIndex, SimpleDirectoryReader
+from IPython.display import Markdown, display
+from llama_index import StorageContext, load_index_from_storage
+
+os.environ["OPENAI_API_KEY"] = "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"
+
+
+documents = SimpleDirectoryReader("docs").load_data()
+
+
+index = VectorStoreIndex.from_documents(documents)
+
+query_engine = index.as_query_engine()
+
+user_query = input("Enter your question: ")
+
+print(query_engine.query(user_query), "Thank you.")
+
+index.storage_context.persist()
+
+storage_context = StorageContext.from_defaults(persist_dir="./storage")
+index = load_index_from_storage(storage_context=storage_context)
